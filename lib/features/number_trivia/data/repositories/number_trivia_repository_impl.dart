4144e961c3a15024c25ee1699faffba576e2c42e
Parmesh Mahore <parmeshtoyou@gmail.com>
2022-08-26 22:58:06 +0530

    update main.dart and number trivia repository

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,9 +1,20 @@
 import 'package:dartz/dartz.dart';
 import 'package:number_trivia_app/core/error/failures.dart';
+import 'package:number_trivia_app/core/platform/network_info.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 class NumberTriviaRepositoryImpl extends NumberTriviaRepository {
+
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl({required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo});
 
   @override
   Future<Either<Failure, NumberTrivia>> getConcreteTrivia(int number) {
